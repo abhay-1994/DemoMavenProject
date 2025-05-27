039d1fa84a350c670b8acf0979e193a7c8b7d1e7
Abhay3011994 <abhay3011994@gmail.com>
2025-05-27 11:54:59 +0530

    changesfromabhaybranch

--- src/test/java/loginModule/Paymentpage.java
+++ src/test/java/loginModule/Paymentpage.java
@@ -1,5 +1,16 @@
 package loginModule;
 
-public class Paymentpage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class Paymentpage {
+	
+
+	public void paymentpage() {
+	
+	WebDriver driver= new ChromeDriver();
+	System.out.println();
+
+	driver.get("https://mvnrepository.com/");
+}
 }
